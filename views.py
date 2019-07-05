f048a369900ebc989c961224974893764ad55858
ztx <1310495560@qq.com>
2019-07-05 08:53:17 +0800

    注册了新功能

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def index(request):
+    return HttpResponse("index")
+
+def login(request):
+    return redirect("/index")
+
+def reg(request):
+    return redirect("reg")
